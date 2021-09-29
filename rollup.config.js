a585e607dbe8db40c7d5011398b5e656d31773d3
Lek Huda <mhmmd.huda@gmail.com>
2021-09-29 22:43:00 +0700

    v1.1.10-beta.3 - remove demo

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,12 @@
 // Plugins
 import commonjs from "@rollup/plugin-commonjs";
+import minifyCss from "./rollup-plugin-node-minify";
+import replace from "@rollup/plugin-replace";
+import strip from "@rollup/plugin-strip";
 import typescript from "@rollup/plugin-typescript";
 import { babel } from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import strip from "@rollup/plugin-strip";
-
-// Dev Plugins
-import alias from "@rollup/plugin-alias";
-import copy from "rollup-plugin-copy";
-import html2 from "rollup-plugin-html2";
-import postcss from "rollup-plugin-postcss";
-import replace from "@rollup/plugin-replace";
-import serve from "rollup-plugin-serve";
 import { terser } from "rollup-plugin-terser";
-import minifyCss from "./rollup-plugin-node-minify";
-
-let builds = [];
 
 const defaultPlugins = [
   replace({
@@ -49,7 +40,7 @@ const distBuildConfig = {
   output: {
     name: "ReactShakaPlayer",
     file: "dist/index.js",
-    format: process.env.DEV ? "esm" : "cjs",
+    format: "cjs",
     exports: "named",
     globals: {
       react: "react",
@@ -60,46 +51,6 @@ const distBuildConfig = {
   plugins: defaultPlugins,
 };
 
-builds.push(distBuildConfig);
-
-if (process.env.DEV) {
-  const devPlugins = [
-    ...defaultPlugins,
-    copy({
-      targets: [
-        {
-          src: "src/styles/ui.css",
-          dest: "dist",
-        },
-      ],
-    }),
-    serve("dist"),
-    html2({
-      fileName: "index.html",
-      template: "demo/index.html",
-    }),
-    postcss(),
-    alias({
-      entries: [
-        {
-          find: "@mkhuda/react-shaka-player",
-          replacement: "../dist/index.js",
-        },
-      ],
-    }),
-  ];
-
-  const devOutputBuildConfig = {
-    input: "demo/index.js",
-    output: {
-      file: "dist/demo.js",
-      format: "esm",
-      exports: "auto",
-    },
-    plugins: devPlugins,
-  };
-
-  builds.push(devOutputBuildConfig);
-}
+const builds = distBuildConfig;
 
 export default builds;
